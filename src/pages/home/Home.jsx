feca981b3d396888c43cbb81e4995aad6d573390
Sanchit Gnawali <sanchitgnawali@gmail.com>
2022-01-05 14:09:47 -0600

    Added pages boilerplate and added react-router-dom

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,8 @@
+import styles from "./Home.module.css";
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,9 @@
+import styles from "./Login.module.css";
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+}
