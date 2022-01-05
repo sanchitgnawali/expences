8f9d361079d1955051490673db762b5e43a738af
Sanchit Gnawali <sanchitgnawali@gmail.com>
2022-01-05 15:08:28 -0600

    added firebase and created custon signup hook

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTN0wcI8J350RN02SonRiSrWTrpNvLNdA",
+  authDomain: "myexpence-a7e3b.firebaseapp.com",
+  projectId: "myexpence-a7e3b",
+  storageBucket: "myexpence-a7e3b.appspot.com",
+  messagingSenderId: "895423412654",
+  appId: "1:895423412654:web:051d04a4baefcc20b2303f",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const fireStore = firebase.firestore();
+const firebaseAuth = firebase.auth();
+
+export { fireStore, firebaseAuth };
---
+++ src/hooks/useSignup.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { firebaseAuth } from "../firebase/config";
+
+export const useSignup = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  const signup = async (email, password, displayName) => {
+    setError(null);
+    isPending(true);
+
+    try {
+      const response = await firebaseAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(response);
+
+      if (!response) {
+        throw new Error("Couldn't sign the user up");
+      }
+
+      //add display name to user
+      await response.user.updateProfile({ displayName });
+      setIsPending(false);
+      setError(null);
+    } catch (error) {
+      console.log(error.message);
+      setError(error.message);
+      setIsPending(false);
+    }
+  };
+
+  return { isPending, error, signup };
+};
