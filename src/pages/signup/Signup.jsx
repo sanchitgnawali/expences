f9dbad920714ac4219ff2d505f19de4425024ddd
Sanchit Gnawali <sanchitgnawali@gmail.com>
2022-01-05 14:41:00 -0600

    Created signup page including styles

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,9 +1,46 @@
-import styles from "./Signup.module.css";
+import { useState } from "react";
+
+//styles
+import "./Signup.css";
 
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+
   return (
-    <div>
-      <h1>Signup</h1>
-    </div>
+    <form onSubmit={handleSubmit} className="signup-form">
+      <h2>sign up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
   );
 }
