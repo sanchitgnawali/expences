79ef3cc37147cb90e6f1085248194dcc3d325e76
Sanchit Gnawali <sanchitgnawali@gmail.com>
2022-01-11 19:56:33 -0600

    Created useCollection custom hook to implement realtime database

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { fireStore } from "../firebase/config";
+
+export const useCollection = (collection) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let ref = fireStore.collection(collection);
+
+    const unsubs = ref.onSnapshot(
+      () => {
+        let results = [];
+        snapshot.docs.forEach((doc) => {
+          results.push({ ...doc.data(), id: doc.id });
+        });
+        //update the state
+        setDocuments(results);
+      },
+      (error) => {
+        console.log(error);
+        setError(error);
+      }
+    );
+
+    return () => unsubs();
+  }, [collection]);
+
+  return { documents, error };
+};
