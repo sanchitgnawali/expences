0e2dd9aa9aa5541283e7902ef730bb0214cf10a5
Sanchit Gnawali <sanchitgnawali@gmail.com>
2022-01-13 23:18:31 -0600

    made the position of a transaction consistent in a transaction list during insertion

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,17 +1,22 @@
 import { useEffect, useRef, useState } from "react";
 import { fireStore } from "../firebase/config";
 
-export const useCollection = (collection, _query) => {
+export const useCollection = (collection, _query, _orderBy) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
   const query = useRef(_query).current;
+  const orderBy = useRef(_orderBy).current;
 
   useEffect(() => {
     let ref = fireStore.collection(collection);
 
     if (query) {
       ref = ref.where(...query);
+    }
+
+    if (orderBy) {
+      ref = ref.orderBy(...orderBy);
     }
 
     const unsubs = ref.onSnapshot(
@@ -30,7 +35,7 @@ export const useCollection = (collection, _query) => {
     );
 
     return () => unsubs();
-  }, [collection, query]);
+  }, [collection, query, orderBy]);
 
   return { documents, error };
 };
